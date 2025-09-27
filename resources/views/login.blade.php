03762abb7f70249fbaf81445a9452cfb0b34940d
jihed.horchani <jihed.horchani@esprit.tn>
2025-09-27 11:51:15 +0100

    add tep

---
+++ resources/views/login.blade.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from themeadapt.com/tf/oxinex/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 26 Sep 2025 21:11:27 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Oxinex - Health Supplement HTML Template</title>
+    <meta name="description" content="Oxinex - Health Supplement HTML Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{Vite::asset('resources/assets/img/favicon.png')}}">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+       @vite(['resources/assets/css/bootstrap.min.css'])
+   @vite(['resources/assets/css/magnific-popup.css'])
+    @vite(['resources/assets/css/swiper-bundle.min.css'])
+  @vite(['resources/assets/css/slick.css'])
+    @vite(['resources/assets/css/default-icons.css'])
+    @vite(['resources/assets/css/default.css'])
+    @vite(['resources/assets/css/sal.css'])
+    @vite(['resources/assets/css/tg-cursor.css'])
+    @vite(['resources/assets/css/main.css'])
+</head>
+
+<body>
+
+    <!--Preloader-->
+    <div id="preloader">
+        <div id="loader" class="loader">
+            <div class="loader-container">
+                <div class="loader-icon"><img src="{{Vite::asset('resources/assets/img/logo/preloader.svg')}}" alt="Preloader"></div>
+            </div>
+        </div>
+    </div>
+    <!--Preloader-end -->
+
+    <!-- Scroll-top -->
+    <button class="scroll__top scroll-to-target" data-target="html">
+        <svg width="14" height="14" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 6L1 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M6 11L1 6L6 1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </button>
+    <!-- Scroll-top-end-->
+
+
+
+    <!-- main-area -->
+    <main class="main-area fix">
+
+        <!-- login-area -->
+        <section class="login__area">
+            <div class="container-fluid p-0">
+                <div class="row gx-0">
+                    <div class="col-md-6">
+                        <div class="login__left-side" data-background="{{Vite::asset('resources/assets/img/bg/sd_bg.html')}}">
+                            <a href="index-2.html"><img src="{{Vite::asset('resources/assets/img/logo/logo.svg')}}" alt="logo"></a>
+                            <div class="login__left-content">
+                                <p>“This software simplifies the website building process, making it a breeze to manage our
+                                    online presence.”</p>
+                                <h4 class="title">David Handerson</h4>
+                                <span>Founder & CEO</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="login__form-wrap">
+                            <h2 class="title">Sign in</h2>
+                            <div class="login__form-social">
+                                <a href="https://github.com/" target="_blank">
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.7447 1.42792H16.2748L10.7473 7.74554L17.25 16.3424H12.1584L8.17053 11.1284L3.60746 16.3424H1.07582L6.98808 9.58499L0.75 1.42792H5.97083L9.57555 6.19367L13.7447 1.42792ZM12.8567 14.828H14.2587L5.20905 2.86277H3.7046L12.8567 14.828Z" fill="currentColor"></path>
+                                    </svg>
+                                </a>
+                                <a href="https://www.facebook.com/" target="_blank">
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9 0C4.02948 0 0 4.02948 0 9C0 13.2206 2.90592 16.7623 6.82596 17.735V11.7504H4.97016V9H6.82596V7.81488C6.82596 4.75164 8.21232 3.3318 11.2198 3.3318C11.79 3.3318 12.7739 3.44376 13.1764 3.55536V6.04836C12.964 6.02604 12.595 6.01488 12.1367 6.01488C10.661 6.01488 10.0908 6.57396 10.0908 8.02728V9H13.0306L12.5255 11.7504H10.0908V17.9341C14.5472 17.3959 18.0004 13.6015 18.0004 9C18 4.02948 13.9705 0 9 0Z" fill="currentColor"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                            <span class="divider">or</span>
+                            <form action="#" class="login__form">
+                                <div class="form__grp">
+                                    <input type="email" placeholder="Your email">
+                                </div>
+                                <div class="form__grp">
+                                    <input type="password" placeholder="Password">
+                                </div>
+                                <div class="account__check">
+                                    <div class="account__check-remember">
+                                        <input type="checkbox" class="form-check-input" value="" id="terms-check">
+                                        <label for="terms-check" class="form-check-label">Remember me</label>
+                                    </div>
+                                    <div class="account__check-forgot">
+                                        <a href="reset.html">Forgot Password?</a>
+                                    </div>
+                                </div>
+                                <button type="submit" class="tg-btn tg-btn-three black-btn">Log in</button>
+                            </form>
+                            <div class="account__switch">
+                                <p>Have no account yet?<a href="register.html">Sign up</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- login-area-end -->
+
+
+    </main>
+    <!-- main-area-end -->
+
+
+
+    <!-- JS here -->
+    @vite(['resources/assets/js/vendor/jquery-3.6.0.min.js'])
+    @vite(['resources/assets/js/bootstrap.min.js'])
+    @vite(['resources/assets/js/jquery.magnific-popup.min.js'])
+    @vite(['resources/assets/js/jquery.appear.js'])
+    @vite(['resources/assets/js/swiper-bundle.min.js'])
+    @vite(['resources/assets/js/slick.js'])
+   @vite(['resources/assets/js/jquery.marquee.min.js'])
+    @vite(['resources/assets/js/jquery.counterup.min.js'])
+    @vite(['resources/assets/js/tg-cursor.min.js'])
+    @vite(['resources/assets/js/jquery.easing.js'])
+    @vite(['resources/assets/js/sal.js'])
+    @vite(['resources/assets/js/ajax-form.js'])
+    @vite(['resources/assets/js/main.js'])
+</body>
+
+
+<!-- Mirrored from themeadapt.com/tf/oxinex/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 26 Sep 2025 21:11:27 GMT -->
+</html>
