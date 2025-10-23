66c281c0fd0be1351141eedd85eb44e9eb8d28ac
jihed.horchani <jihed.horchani@esprit.tn>
2025-10-23 13:42:36 +0100

    fix docker compose

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
+                'resources/css/app.css',
                 'resources/css/sh-sidebar.css',
                 'resources/css/meals-filters.css',
                 'resources/css/meal-plan-forms.css',
@@ -13,12 +14,18 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/sh-sidebar.js',
                 'resources/js/meal-ingredients.js',
-                'resources/js/meal-plan-form.js'
+                'resources/js/meal-plan-form.js',
+                'resources/assets/img/slider/slider_bg01.jpg' // << Add your images here
             ],
             refresh: true,
         }),
     ],
     css: {
         devSourcemap: true,
-    }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources'),
+        },
+    },
 });
