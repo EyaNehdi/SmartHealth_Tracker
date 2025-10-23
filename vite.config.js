af9ddff045dae9c007b6f241eedee7f585a2d989
jihed.horchani <jihed.horchani@esprit.tn>
2025-10-23 13:54:31 +0100

    fix docker compose

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/assets/js/app.js',
+                'resources/assets/css/app.css',
+                ...images, // include all images
                 ...cssFiles,
                 ...jsFiles,
                 ...imageFiles,
