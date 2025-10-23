06eeb6f7932180f418d70ce6e8c0cef46157830d
jihed.horchani <jihed.horchani@esprit.tn>
2025-10-23 14:03:51 +0100

    fix docker compose

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,18 @@ import glob from 'fast-glob';
 // automatically include all CSS, JS, and images
 const cssFiles = glob.sync('resources/assets/css/**/*.css');
 const jsFiles = glob.sync('resources/assets/js/**/*.js');
+const cssfiles1 = glob.sync('resources/css/**/*.css');
+const jsFiles1 = glob.sync('resources/js/**/*.js');
 const imageFiles = glob.sync('resources/assets/img/**/*.{png,jpg,jpeg,gif,svg}');
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/assets/js/app.js',
-                'resources/assets/css/app.css',
+                'resources/js/app.js',
+                'resources/css/app.css',
+                ...cssfiles1,
+                ...jsFiles1,
                 ...cssFiles,
                 ...jsFiles,
                 ...imageFiles, // use imageFiles here
